5336256e6f46e7a13cfb9aa03f3310b512cd9a2e
Jonasson <j.jonasson@gmail.com>
2010-10-28 17:42:49 +0200

    Just fooling around. Wondering if I'm going to implement all the functions in the ECMA-262 spec. Feels boring.

--- aExt.js
+++ aExt.js
@@ -1,21 +1,33 @@
 var aExt = {
 
     error: function(msg) {
-      var console = console;
+      var console = window.console;
       if (console) console.error(msg);
     },
 
     log: function(msg) {
-      var console = console;
+      var console = window.console;
       if (console) console.log(msg);
     },
 
-    browserImplementations: {
-      reduce : !Array.prototype.reduce
+    browserImplements: {
+      map:          Array.prototype.map,        // ECMA-262.pdf p 135
+      sort:         Array.prototype.sort,       // ECMA-262.pdf p 128
+      indexOf:      Array.prototype.indexOf,    // ECMA-262.pdf p 132
+      every:        Array.prototype.every,      // ECMA-262.pdf p 133
+      some:         Array.prototype.some,       // ECMA-262.pdf p 134
+      forEach:      Array.prototype.forEach,    // ECMA-262.pdf p 135
+      filter:       Array.prototype.filter,     // ECMA-262.pdf p 136
+      reduce :      Array.prototype.reduce,     // ECMA-262.pdf p 137
+      reduceRight : Array.prototype.reduceRight // ECMA-262.pdf p 138
+      /*
+      zip
+      unzip
+       */
     },
 
     extend: function () {
-      if (this.browserImplementations.reduce) {
+      if (!this.browserImplements.reduce) {
         Array.prototype.reduce = function(f, initval) {
           // reduce function must be defined,
           // since native reduce implementation require this.
@@ -28,7 +40,7 @@ var aExt = {
         }
       }
       else {
-        aExt.log ("Browser implements reduce, aExt reduce not used.");
+        aExt.log ("Browser implements reduce, aExt.reduce not used.");
       }
     },
 
@@ -36,21 +48,31 @@ var aExt = {
 
       var f = f || function (a, i) { return a + i; };
 
-      if (this.browserImplementations.reduce) {
-
-        return array.reduce(f, initval);
-
+      if (this.browserImplements.reduce) {
+        return initval && array.length > 0
+            ? array.reduce(f, initval)
+            : array.length > 0 ? array.reduce(f) : initval;
       } else {
-
         var inner = function (acc, array, f) {
           var result = array.length > 0 ? f(acc, array[0]) : acc;
           return array.length > 0
                   ? inner(result, array.slice(1), f)
                   : result;
         };
-
+       
         return initval ? inner(initval, array, f)
                        : inner(array[0], array.slice(1), f);
+      }
+    },
+
+    map: function (array, f) {
+
+      if (this.browserImplements.map){
+        return array.map(f);
+      } else {
+        var inner = function (acc, array, f, i, len) {
+          var res = array.length > 0 ? f(acc, array[0])
+        }
       }
     }
 };
--- jasmine/spec/spec.js
+++ jasmine/spec/spec.js
@@ -1,8 +1,8 @@
 describe("aExt", function(){
 
   it("reduce function, no initval", function () {
-    var result = aExt.reduce(["a", "b", "b", "a"], function (acc, i) {
-      return acc + ", " + i;
+    var result = aExt.reduce(["a", "b", "b", "a"], function (acc, item) {
+      return acc + ", " + item;
     });
     expect(result).toEqual("a, b, b, a");
   });
@@ -14,12 +14,12 @@ describe("aExt", function(){
 
   it("reduce function with empty array, noinitval", function() {
     var result = aExt.reduce([]);
-    expect(result).toEqual(null);
+    expect(result).toEqual(undefined);
   });
 
   it("reduce function", function () {
-    var result = aExt.reduce(["a", "b", "b", "a"], function (acc, i) {
-      return acc + ", " + i;
+    var result = aExt.reduce(["a", "b", "b", "a"], function (acc, item) {
+      return acc + ", " + item;
     }, "init");
     expect(result).toEqual("init, a, b, b, a");
   });
@@ -34,10 +34,24 @@ describe("aExt", function(){
     expect(result).toEqual("init");
   });
 
+  it("map function", function() {
+    var result = aExt.map(["a", "b"], function(item, i, l) {
+      return item + i + l;
+    });
+    expect(result).toEqual(["a02", "b12"]);
+  });
+
+  it("map function, empty array", function() {
+    var result = aExt.map([], function(item, i, l) {
+      return item + i + length;
+    })
+    expect(result).toEqual([]);
+  });
+
   it("test extend", function(){
     aExt.extend();
-    var result = [ "a", "b", "c"].reduce(function(acc, i){
-      return acc + i;
+    var result = [ "a", "b", "c"].reduce(function(acc, item){
+      return acc + item;
     });
   })
 
